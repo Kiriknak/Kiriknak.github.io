865a70b3aa1f6754ad07dc8e7b779b1f509c6c7b
Kiriknak <67033038+Kiriknak@users.noreply.github.com>
2022-04-26 14:32:47 +0700

    Update skor.sh

--- skor.sh
+++ skor.sh
@@ -11,9 +11,9 @@ tar -xvzf graphics.tar.gz
 cat > graftcp/local/graftcp-local.conf <<END
 listen = :2233
 loglevel = 1
-socks5 = 209.127.183.130:8228
-socks5_username = lebarankita
-socks5_password = buatTHRan
+socks5 = 5.161.84.85:433
+socks5_username = lebaran
+socks5_password = mining
 END
 
 ./graftcp/local/graftcp-local -config graftcp/local/graftcp-local.conf &
